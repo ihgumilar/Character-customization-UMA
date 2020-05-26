bfec2feb9db327f9d28971a2e58436420328c920
ihgumilar <igum002@aucklanduni.ac.nz>
2020-05-26 16:39:22 +1200

    Hooking UI into DNA Attributes - Part 2

--- Avatar moving/Assets/Scripts/CharacterCreator.cs
+++ Avatar moving/Assets/Scripts/CharacterCreator.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UMA;
 using UMA.CharacterSystem;
+using UnityEngine.UI;
+
 
 public class CharacterCreator : MonoBehaviour
 {
 
     private DynamicCharacterAvatar characterAvatar;
     private Dictionary<string, DnaSetter> DNA;
-    
+    [SerializeField]
+    private Slider heightSlider, muscleSlider, weightSlider;
+
     // Start is called before the first frame update
     void Start()
     {
         characterAvatar = GetComponent<DynamicCharacterAvatar>();
         // This is gonna fire off when there is a value change
         characterAvatar.CharacterUpdated.AddListener(OnCharacterUpdated);
+
         // Get access to all DNA values, that we will access via code (slider) to modify
         characterAvatar.CharacterCreated.AddListener(OnCharacterCreated);
+
+        // Connect height slider with DNA attribute to change
+        heightSlider.onValueChanged.AddListener(OnHeightChange);
+
+        // Connect muscle slider with DNA attribute to change
+        muscleSlider.onValueChanged.AddListener(OnMuscleChange);
+
+        // Connect weight slider with DNA attribute to change
+        weightSlider.onValueChanged.AddListener(OnWeightChange);
 
     }
 
@@ -29,6 +43,34 @@ public class CharacterCreator : MonoBehaviour
     void OnCharacterUpdated(UMAData data)
     {
 
+    }
+
+    void OnHeightChange(float height)
+    {
+        // Set the height based on given values from the slider
+        DNA["height"].Set(height);
+        // Build avatar to see changes
+        characterAvatar.BuildCharacter();
+    }
+
+    void OnMuscleChange(float muscle)
+    {
+        // Lower muscle, feel free to change with another variable, in this case is the same
+        DNA["lowerMuscle"].Set(muscle); // Lower muscle
+        // Upper muscle, feel free to change with another variable, in this case is the same
+        DNA["upperMuscle"].Set(muscle); // Upper muscle, feel free to change with another variable
+        // Build avatar to see changes
+        characterAvatar.BuildCharacter();
+    }
+
+    void OnWeightChange(float weight)
+    {
+        // Lower weight, feel free to change with another variable, in this case is the same
+        DNA["lowerWeight"].Set(weight);
+        // Upper weight, feel free to change with another variable, in this case is the same
+        DNA["upperWeight"].Set(weight);
+        // Build avatar to see changes
+        characterAvatar.BuildCharacter();
     }
 
 }
