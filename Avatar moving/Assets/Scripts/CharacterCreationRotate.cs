ea4dc968a6748dc77c98db85fdee7b9e928a684e
ihgumilar <igum002@aucklanduni.ac.nz>
2020-05-26 15:50:20 +1200

    Hooking UI into DNA Attributes - Part 1

---
+++ Avatar moving/Assets/Scripts/CharacterCreationRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreationRotate : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        transform.Rotate(Vector3.up * (Input.GetAxisRaw("Mouse X") * -5f));
+    }
+
+    private void OnMouseUp()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        
+    }
+}
---
+++ Avatar moving/Assets/Scripts/CharacterCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+
+public class CharacterCreator : MonoBehaviour
+{
+
+    private DynamicCharacterAvatar characterAvatar;
+    private Dictionary<string, DnaSetter> DNA;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterAvatar = GetComponent<DynamicCharacterAvatar>();
+        // This is gonna fire off when there is a value change
+        characterAvatar.CharacterUpdated.AddListener(OnCharacterUpdated);
+        // Get access to all DNA values, that we will access via code (slider) to modify
+        characterAvatar.CharacterCreated.AddListener(OnCharacterCreated);
+
+    }
+
+    void OnCharacterCreated(UMAData data)
+    {
+        DNA = characterAvatar.GetDNA();
+    }
+
+    void OnCharacterUpdated(UMAData data)
+    {
+
+    }
+
+}
