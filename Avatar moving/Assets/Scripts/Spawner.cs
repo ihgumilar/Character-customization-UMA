648b4e0a81aa34a1e72008c9cbd86fdee3f8b62c
ihgumilar <igum002@aucklanduni.ac.nz>
2020-05-26 12:32:25 +1200

    Building character creator UI (Panel to adjust character)

---
+++ Avatar moving/Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gumilar.VR
+{
+    public class Spawner : MonoBehaviour
+    {
+        void Spawn(Color color)
+        {
+            // Create a sphere
+            GameObject sphere = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere));
+
+            // Adjust transform
+            sphere.transform.localScale *= 0.05f;
+            sphere.transform.position = transform.position + transform.forward * 0.3f;
+
+            // Set a color of sphere
+            sphere.GetComponent<Renderer>().material.color = color;          
+
+        }
+
+        public void SpawnRed()
+        {
+            Spawn(Color.red);
+        }
+
+        public void SpawnGreen()
+        {
+            Spawn(Color.green);
+        }
+
+        public void SpawnBlue()
+        {
+            Spawn(Color.blue);
+        }
+
+        public void SpawnYellow()
+        {
+            Spawn(Color.yellow);
+        }
+    }
+}
