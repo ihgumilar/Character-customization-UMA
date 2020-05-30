c68333cc1bf237a2c619a6e67a23b15e3edfa874
ihgumilar <igum002@aucklanduni.ac.nz>
2020-05-30 12:06:37 +1200

    Load saved avatar (without function) & Running around

--- Avatar moving/Assets/Scripts/Player.cs
+++ Avatar moving/Assets/Scripts/Player.cs
@@ -7,14 +7,27 @@ using UMA.CharacterSystem;
 public class Player : MonoBehaviour
 {
     public DynamicCharacterAvatar characterAvatar;
+    private Animator animator;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
         characterAvatar = GetComponent<DynamicCharacterAvatar>();
         //characterAvatar.CharacterCreated.AddListener(OnCharacterCreated);
+
+        // If I put the following two lines into a function and called from the line that was commented above,
+        // it doesn't load the SAVED avatar. With this current structure, it can load the avatar (without calling function).
         characterAvatar.LoadFromRecipeString(PlayerPrefs.GetString("CharacterData"));
         characterAvatar.BuildCharacter();
+
+        animator = GetComponent<Animator>();    
+        
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", Input.GetAxis("Vertical"));
+        animator.SetFloat("Direction", Input.GetAxis("Horizontal"));
     }
 
     //void OnCharacterCreated(UMAData data)
